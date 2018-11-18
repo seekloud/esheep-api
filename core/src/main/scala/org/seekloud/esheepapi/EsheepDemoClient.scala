8c9ef2ad9733eee3c055f54c56982a65d5813a73
Taoz <zhangtao@neotel.com.cn>
2018-11-18 12:03:02 +0800

    server to go.

---
+++ core/src/main/scala/org/seekloud/esheepapi/EsheepDemoClient.scala
@@ -0,0 +1,10 @@
+package org.seekloud.esheepapi
+
+/**
+  * User: Taoz
+  * Date: 11/18/2018
+  * Time: 12:02 PM
+  */
+class EsheepDemoClient {
+
+}
---
+++ core/src/main/scala/org/seekloud/esheepapi/EsheepDemoServer.scala
@@ -0,0 +1,10 @@
+package org.seekloud.esheepapi
+
+/**
+  * User: Taoz
+  * Date: 11/18/2018
+  * Time: 12:01 PM
+  */
+class EsheepDemoServer {
+
+}
