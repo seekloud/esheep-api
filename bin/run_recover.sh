487995cbc24692d7abda3d5a7f47026b66421545
Tao Zhang <zta@outlook.com>
2019-04-11 12:29:45 +0800

    recover script: run_recover.sh bug fix.

--- bin/run_recover.sh
+++ bin/run_recover.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+##version 2019.4.11
 
 echo "`date`: run_recover begin."
 
@@ -7,22 +8,19 @@ TARGET_CMD=$1
 PARAMETERS=${@:2}
 need_continue=1
 
-current_time=`date`
-echo ${current_time}": run cmd[$TARGET_CMD] with parameters[$PARAMETERS]"
-eval exec "\"${TARGET_CMD}\"" ${PARAMETERS}
-exit_code=$?
-while [ ${exit_code} -ne 0 ]
+exit_code=1
+
+while [[ ${exit_code} -ne 0 ]]
 do 
   current_time=`date`
-  echo "$current_time: recover cmd[$TARGET_CMD] with parameters[$PARAMETERS]"
-  eval exec "\"${TARGET_CMD}\"" ${PARAMETERS}
+  echo "$current_time:run recover start: cmd[$TARGET_CMD] with parameters[$PARAMETERS]"
+  eval "\"${TARGET_CMD}\"" ${PARAMETERS}
   exit_code=$?
   sleep 3
 done
 
 current_time=`date`
-
-echo "$current_time: FINISH"
+echo "$current_time:run recover finish: cmd[$TARGET_CMD] with parameters[$PARAMETERS]"
 
 
 
